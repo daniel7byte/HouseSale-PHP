997ee2706de83dc4eebf47c90839e7cd46e0f2d6
Jose Alvarado <jose_alvarado17@hotmail.com>
2017-01-29 13:59:42 -0400

    Glosario de idiomas creado en /lang para aplicar el sistema multilenguaje

---
+++ lang/en.php
@@ -0,0 +1,51 @@
+<?php
+
+/* ----------------------------------------------------------------------
+*   MENU/NAVBAR
+*  ------------------------------------------------------------------- */
+define("menu_item1", "HOME");
+define("menu_item2", "OUR COMPANY");
+define("menu_item3", "SELL");
+define("menu_item4", "CONTACT US");
+define("menu_item5", "ESP");
+/* ----------------------------------------------------------------------
+*   HEADER
+*  ------------------------------------------------------------------- */
+define("header_title", "FIND YOUR NEXT HOME HERE");
+define("header_content", "We have more than 30.000 homes available for you in the state of Georgia. Choose your best option at a price that is right for you and you will save 1.5% with us.");
+define("header_cta", "Get started");
+/* ----------------------------------------------------------------------
+*   FORM
+*  ------------------------------------------------------------------- */
+define("form_label1", "State");
+define("form_label2", "ZIP Code");
+define("form_label3", "County");
+define("form_label4", "City");
+define("form_label5", "Date range");
+define("form_label6", "Price range");
+define("form_label7", "System");
+define("form_btn1", "Reset");
+define("form_btn2", "Search");
+define("form_select_all","All");
+/* ----------------------------------------------------------------------
+*   HOME SECTION
+*  ------------------------------------------------------------------- */
+define("home_subtitle1", "Hot sales");
+define("home_subtitle2", "WHY SHOULD YOU USE JOYGLE");
+define("home_text1", "We specialize in providing excellent advice for you to help you decide on the home you always dreamed of.");
+define("home_text2", "Our efficient platform provides all the necessary tools for you to buy/sell real estate the best and fastest way. with us you’ll get the following benefits:");
+define("home_benefit1_title", "BIG SAVINGS");
+define("home_benefit2_title", "WE FACILITATE SALES");
+define("home_benefit3_title", "USER FRIENDLY");
+define("home_benefit4_title", "VARIOUS OPTIONS");
+define("home_benefit1_description", "Our commission is one of the lowest in the market: 4.5%.");
+define("home_benefit2_description", "We take care of your real estate in order for you to have a piece of mind.");
+define("home_benefit3_description", "We are transforming the way consumers make home-related decisions and connect with professionals.");
+define("home_benefit4_description", "It starts with our living database of more than 110 million U.S. homes – including homes for sale, homes for rent and homes not currently on the market.");
+define("home_contact_title", "We Buy and Sell Your Property");
+define("home_contact_btn","Contact us");
+define("home_allies_title_slim", "GET TO KNOW ");
+define("home_allies_title_bold", "OUR STRATEGIC ALLIES");
+define("home_allies_description", "We work with you to provide you with a better experience.");
+
+?>
---
+++ lang/es.php
@@ -0,0 +1,51 @@
+<?php
+
+/* ----------------------------------------------------------------------
+*   MENU/NAVBAR
+*  ------------------------------------------------------------------- */
+define("menu_item1", "Inicio");
+define("menu_item2", "Nuestra Compañía");
+define("menu_item3", "Venta");
+define("menu_item4", "Contáctenos");
+define("menu_item5", "ENG");
+/* ----------------------------------------------------------------------
+*   HEADER
+*  ------------------------------------------------------------------- */
+define("header_title", "Encuentre su próximo hogar aquí");
+define("header_content", "Tenemos disponible para usted más de 30.000 propiedades en el estado de Georgia para que elija su mejor opción a un precio más favorable. Ahorre 1.5% con nosotros");
+define("header_cta", "Buscar aquí");
+/* ----------------------------------------------------------------------
+*   FORM
+*  ------------------------------------------------------------------- */
+define("form_label1", "Estado");
+define("form_label2", "Código de área");
+define("form_label3", "Condado");
+define("form_label4", "Ciudad");
+define("form_label5", "Rango de fecha");
+define("form_label6", "Rango de precio");
+define("form_label7", "Plataforma");
+define("form_btn1", "Borrar");
+define("form_btn2", "Buscar");
+define("form_select_all","Todos");
+/* ----------------------------------------------------------------------
+*   HOME SECTION
+*  ------------------------------------------------------------------- */
+define("home_subtitle1", "Ventas recientes");
+define("home_subtitle2", "POR QUÉ USAR JOYGLE");
+define("home_text1", "We specialize in providing excellent advice for you to help you decide on the home you always dreamed of.");
+define("home_text2", "Nuestra plataforma eficiente brinda todas las herramientas necesarias para que usted pueda comprar ó vender propiedades raíces de la mejor manera. Con nosotros usted obtendrá los siguientes beneficios:");
+define("home_benefit1_title", "AHORRO DE DINERO");
+define("home_benefit2_title", "Facilitamos la venta");
+define("home_benefit3_title", "Fácil de Usar");
+define("home_benefit4_title", "Diversas opciones");
+define("home_benefit1_description", "Nuestra comisión es una de las más económicas del mercado: 4.5% .");
+define("home_benefit2_description", "Nos encargamos de gestionar su bien raíz para su tranquilidad.");
+define("home_benefit3_description", "Nuestro buscador simple ofrece diversos filtros para encontrar su vivienda de acuerdo a su necesidad.");
+define("home_benefit4_description", "Tenemos más de 30.000 Opciones de vivienda disponibles en el mercado.");
+define("home_contact_title", "Compramos y vendemos su vivienda");
+define("home_contact_btn","Contáctenos");
+define("home_allies_title_slim", "CONOZCA NUESTROS");
+define("home_allies_title_bold", "ALIADOS ESTRATÉGICOS");
+define("home_allies_description", "Trabajamos en conjunto para brindarle una mejor experiencia.");
+
+?>
